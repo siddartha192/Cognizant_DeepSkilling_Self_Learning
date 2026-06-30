508e975cebae6f2cae5798dcf202d9ee227620cd
Siddhu <polepakasiddartha19@gmail.com>
2026-06-30 17:03:31 +0530

    week 2 into repo

---
+++ Hand_On/Week_2/Spring Core and Maven/Exercide_2/LibraryManagementSystemModified/src/main/java/org/example/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+public class BookRepository {
+    public void displayRepository() {
+        System.out.println("BookRepository object created");
+    }
+}
---
+++ Hand_On/Week_2/Spring Core and Maven/Exercise_1/LibraryManagementSystem/src/main/java/org/example/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+public class BookRepository {
+    public void displayRepository() {
+        System.out.println("Book Repository Bean Created");
+    }
+}
