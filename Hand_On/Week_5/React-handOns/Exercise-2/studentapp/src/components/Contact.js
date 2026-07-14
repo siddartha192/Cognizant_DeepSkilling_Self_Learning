cee81f6ab626cda7dd827c524dc9f9e3c38d8355
Siddhu <polepakasiddartha19@gmail.com>
2026-07-14 13:28:56 +0530

    initial commit for exercise-2 in react

---
+++ Hand_On/Week_5/React-handOns/Exercise-2/studentapp/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Contact extends Component{
+    render(){
+        return(
+            <div className="container mt-4">
+                <h3>
+                    Welcome to the Contact page of Student Management portal
+                </h3>
+            </div>
+        );
+    }
+}
+
+export default Contact;
